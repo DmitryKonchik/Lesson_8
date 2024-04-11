35d623c212a54c2f89cf9a5daf97b353c24f4c1a
Dmitry <dkonchik@gmail.com>
2024-04-11 19:14:21 +0300

    Hard Task

--- src/Dog.java
+++ src/Dog.java
@@ -1,7 +1,10 @@
 public class Dog extends Animal{
+
+
+    private Dog() {}          // Приватный конструктор, который запрещает создание класса
+
     @Override
     public void voice() {
-        super.voice();
         System.out.println("Собака гавкает");
     }
 
@@ -13,5 +16,17 @@ public class Dog extends Animal{
         } else {
             System.out.println("Собака хочет мяса");
         }
+    }
+    // Статические методы для доп задания
+    public static void eat1(String food) {
+        System.out.println("Собака кушает " + food);
+        if ( food.equalsIgnoreCase("meat")) {
+            System.out.println("Собака довольна");
+        } else {
+            System.out.println("Собака хочет мяса");
+        }
+    }
+    public static void voice1() {
+        System.out.println("Собака гавкает");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Animal tiger = new Tiger();
+        Animal rabbit = new Rabbit();
+        tiger.eat("Meat");
+        tiger.eat("meat");
+        tiger.eat("grass");
+        tiger.voice();
+        rabbit.voice();
+        rabbit.eat("grass");
+
+        //
+        Dog.eat1("Meat");
+        Dog.voice1();
+
     }
 }
